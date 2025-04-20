2594201670ef7b6566b53e0b649b3f1a724ebd31
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-04-20 15:14:37 +0530

    Added Login and Signup page

--- app/src/main/java/com/example/nimbus/splash.java
+++ app/src/main/java/com/example/nimbus/splash.java
@@ -25,7 +25,7 @@ public class splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash.this, MainActivity.class);
+                Intent intent = new Intent(splash.this, register.class);
                 startActivity(intent);
                 finish();
             }
