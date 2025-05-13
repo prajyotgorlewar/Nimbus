4fdf7e58f29889d06751cfe23999010c109a2c9c
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-05-13 21:49:16 +0530

    Update splash.java

--- app/src/main/java/com/example/nimbus/splash.java
+++ app/src/main/java/com/example/nimbus/splash.java
@@ -8,8 +8,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class splash extends AppCompatActivity {
+
+    ImageView logo;
+    TextView name;
+    Animation topAnimation, bottomAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +30,17 @@ public class splash extends AppCompatActivity {
             return insets;
         });
 
+        logo = findViewById(R.id.logoImg);
+        name = findViewById(R.id.logoName);
+        topAnimation = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        logo.setAnimation(topAnimation);
+        name.setAnimation(bottomAnimation);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(splash.this, register.class);
+                Intent intent = new Intent(splash.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
