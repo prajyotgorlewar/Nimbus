b8ddf7e133ac7b0184956ebe111bf13509c74bd6
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-05-15 18:22:22 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/nimbus/MainActivity.java
+++ app/src/main/java/com/example/nimbus/MainActivity.java
@@ -4,17 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
-
+    RecyclerView mainUserRecycler;
+    UserAdapter adapter;
+    FirebaseDatabase database;
+    ArrayList<users> usersArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +39,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        
+
+        database = FirebaseDatabase.getInstance();
         auth = FirebaseAuth.getInstance();
+
+        DatabaseReference reference = database.getReference().child("user");
+        usersArrayList = new ArrayList<>();
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
+
+        mainUserRecycler = findViewById(R.id.mainUserRecycler);
+        mainUserRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mainUserRecycler.setAdapter(adapter);
+
         if(auth.getCurrentUser() == null)
         {
             Intent intent = new Intent(MainActivity.this, login.class);
