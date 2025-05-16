3e189c5dc329e7baa55b9b6730811ea169247e36
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-05-16 22:14:36 +0530

    Update UserAdapter.java

--- app/src/main/java/com/example/nimbus/UserAdapter.java
+++ app/src/main/java/com/example/nimbus/UserAdapter.java
@@ -1,17 +1,28 @@
 package com.example.nimbus;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.viewholder>{
+
+    MainActivity mainActivity;
+    ArrayList<users> usersArrayList;
+
+    public UserAdapter(MainActivity mainActivity, ArrayList<users> usersArrayList) {
+        this.mainActivity = mainActivity;
+        this.usersArrayList = usersArrayList;
+    }
 
     @NonNull
     @Override
     public UserAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.
     }
 
     @Override
