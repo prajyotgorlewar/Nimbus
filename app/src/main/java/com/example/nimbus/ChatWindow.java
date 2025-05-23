ba21241d88a89bd3fdba36e68b629d3a4e8bb987
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-05-23 22:51:35 +0530

    updated chatActivity

--- app/src/main/java/com/example/nimbus/ChatWindow.java
+++ app/src/main/java/com/example/nimbus/ChatWindow.java
@@ -1,13 +1,29 @@
 package com.example.nimbus;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -16,6 +32,13 @@ public class ChatWindow extends AppCompatActivity {
     String receiverName, receiverUid, receiverImg, senderUid;
     CircleImageView profile;
     TextView receiverNameText;
+    CardView sendBtn;
+    EditText messageText;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase database;
+
+    public static String senderImage;
+    public static String receiverImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +58,9 @@ public class ChatWindow extends AppCompatActivity {
         profile = findViewById(R.id.chatProfileImg);
         receiverNameText = findViewById(R.id.receivername);
 
+        messageText = findViewById(R.id.messageText);
+        sendBtn = findViewById(R.id.sendBtn);
+
         try {
             int avatarSerial = Integer.parseInt(receiverImg);
             int avatarResId = AvatarUtils.getAvatarResource(avatarSerial);
@@ -44,7 +70,43 @@ public class ChatWindow extends AppCompatActivity {
             profile.setImageResource(R.drawable.avatar_1);
         }
 
-        receiverNameText.setText(""+receiverName);
+        receiverNameText.setText("" + receiverName);
+        senderUid = firebaseAuth.getUid();
+        DatabaseReference reference = database.getReference().child("users").child(firebaseAuth.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                senderImage = snapshot.child("profilePic").getValue().toString();
+                receiverImage = receiverImg;
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageText.getText().toString();
+                if (message.isEmpty()) {
+                    Toast.makeText(ChatWindow.this, "Type something first!", Toast.LENGTH_SHORT).show();
+                }
+                messageText.setText("");
+                Date date = new Date();
+                messageModel messageModel = new messageModel(message, senderUid, date.getTime());
+                database = FirebaseDatabase.getInstance();
+                database.getReference().child("chats").child("senderRoom").child("messages").push().setValue(messageModel)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                
+                            }
+                        });
+            }
+        });
 
     }
 }
