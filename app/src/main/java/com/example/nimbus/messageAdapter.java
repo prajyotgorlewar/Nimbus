2ee1b7bc682b8c7e1ac3eaca02c968f9faaf211b
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-05-25 23:01:29 +0530

    implemented sender receiver layout logic

--- app/src/main/java/com/example/nimbus/messageAdapter.java
+++ app/src/main/java/com/example/nimbus/messageAdapter.java
@@ -1,16 +1,37 @@
 package com.example.nimbus;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class messageAdapter extends RecyclerView.Adapter{
+
+    Context context;
+    ArrayList<messageModel> messageAdapterArrayList;
+    int ITEM_SEND=1;
+    int ITEM_RECEIVE = 2;
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if(viewType == ITEM_SEND){
+            View view = LayoutInflater.from(context).inflate(R.layout.sender_layout, parent, false);
+            return new senderViewHolder(view);
+        }
+        else {
+            View view = LayoutInflater.from(context).inflate(R.layout.receiver_layout, parent, false);
+            return new senderViewHolder(view);
+        }
     }
 
     @Override
@@ -24,14 +45,22 @@ public class messageAdapter extends RecyclerView.Adapter{
     }
 
     class senderViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView circleImageView;
+        TextView messageText;
         public senderViewHolder(@NonNull View itemView) {
             super(itemView);
+            circleImageView = itemView.findViewById(R.id.senderProfileImg);
+            messageText = itemView.findViewById(R.id.senderMessageText);
         }
     }
 
     class receiverVeiwHolder extends RecyclerView.ViewHolder {
+        CircleImageView circleImageView;
+        TextView messageText;
         public receiverVeiwHolder(@NonNull View itemView) {
             super(itemView);
+            circleImageView = itemView.findViewById(R.id.receiverProfileImg);
+            messageText = itemView.findViewById(R.id.receiverMessageText);
         }
     }
 
